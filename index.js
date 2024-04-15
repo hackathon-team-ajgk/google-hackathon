0288807767ed40d87d8bffc97be2d45e2747eb06
Ernesto-B <ernesto.barreto@ucalgary.ca>
2024-04-14 20:31:55 -0600

    Added template to know if API is working correctly

--- index.js
+++ index.js
@@ -4,8 +4,23 @@
 require("dotenv").config();
 const API_KEY = process.env.GEMINI_API;
 
-import {GoogleGenerativeAI as googleGen} from "@google/generative-ai";
+// import {GoogleGenerativeAI} from "@google/generative-ai";
 
-const genAI = new googleGen(API_KEY);
-const model = genAI.getGenerativeModel({model: "gemini-pro"})
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+// Access your API key (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+  const prompt = "Write a story about a magic backpack."
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
