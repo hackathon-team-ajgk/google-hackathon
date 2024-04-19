c34771c2062cf2c7988e36dc16ea3992e21674b3
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-04-19 14:03:53 -0600

    added components for user auth and home

--- my-movie-list/src/App.js
+++ my-movie-list/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import NoPage from "./components/NoPage";
 
 function App() {
   return (
-    <>
-      <h1>MyMovieList</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="*" element={<NoPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ my-movie-list/src/components/Home.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function Home() {
+  const nav = useNavigate();
+
+  const routeToLogin = () => {
+    nav("/login");
+  };
+
+  const routeToRegister = () => {
+    nav("/register");
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users");
+      console.log(response.data);
+    } catch (error) {
+      // Handle error
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        console.error("Response Error:", error.response.data);
+        console.error("Status Code:", error.response.status);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error("Request Error:", error.request);
+      } else {
+        // Something else happened while setting up the request
+        console.error("Error:", error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>MyMovieList</h1>
+      <div>
+        <button onClick={routeToLogin}>Login</button>
+        <button onClick={routeToRegister}>Register</button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
