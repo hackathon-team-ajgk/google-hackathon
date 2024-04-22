1527aba52d71e522aca7cd43e48fad4701e8d49d
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-04-22 15:32:04 -0600

    added navbar code., not finished

--- my-movie-list/src/App.js
+++ my-movie-list/src/App.js
@@ -5,15 +5,18 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import NoPage from "./components/NoPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="*" element={<NoPage />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="*" element={<NoPage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ my-movie-list/src/components/NavbarData.js
@@ -0,0 +1,18 @@
+export const NavbarData = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Movies",
+    path: "/movies",
+  },
+  {
+    name: "Your List",
+    path: "/your-list",
+  },
+  {
+    name: "Help",
+    path: "/help",
+  },
+];
