fd46546f9bf7b278c5a110610e20e90ccb481e22
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-04-25 13:37:29 -0600

    feature: finished movie on click, subject to changes in the future.

--- my-movie-list/src/components/MovieView.js
+++ my-movie-list/src/components/MovieView.js
@@ -10,30 +10,34 @@ function MovieView({ movieInfo, toggleOverlay }) {
           alt="movie cover"
           id="movie-view-image"
         />
-        <div id="movie-view-text-container" className="text-container">
+        <div id="movie-view-title-overview" className="text-container">
           <p id="movie-view-title" className="movie-text">
             {movieInfo.title}
           </p>
           <p id="movie-overview" className="movie-text">
-            <strong>Overview:</strong> {movieInfo.overview}
+            {movieInfo.overview}
           </p>
-          <p id="movie-genres" className="movie-text">
-            <strong>Genres:</strong> {movieInfo.genreNames}
-          </p>
-          <p id="movie-average-rating" className="movie-text">
-            <strong>Average Rating:</strong> {movieInfo.averageRating}
-          </p>
-          <p id="movie-average-rating" className="movie-text">
-            <strong>Release Date:</strong> {movieInfo.releaseDate}
-          </p>
-          <p id="movie-user-rating" className="movie-text">
-            <strong>User Rating:</strong>
-            {movieInfo.userRating !== "NULL" ? (
-              <>{movieInfo.userRating}</>
-            ) : (
-              <input type="number" id="movie-rating-input" />
-            )}
-          </p>
+          <div id="other-movie-information" className="text-container">
+            <p id="movie-genres" className="movie-text">
+              <strong>Genres:</strong> {movieInfo.genreNames}
+            </p>
+            <p id="movie-average-rating" className="movie-text">
+              <strong>Average Rating:</strong> {movieInfo.averageRating}
+            </p>
+            <p id="movie-average-rating" className="movie-text">
+              <strong>Release Date:</strong> {movieInfo.releaseDate}
+            </p>
+            <p id="movie-user-rating" className="movie-text">
+              <label id="movie-rating-label" htmlFor="movie-rating-input">
+                User Rating:
+              </label>
+              {movieInfo.userRating !== "NULL" ? (
+                <>{movieInfo.userRating}</>
+              ) : (
+                <input type="number" max="10" id="movie-rating-input" />
+              )}
+            </p>
+          </div>
         </div>
         <div id="close-button-div" onClick={toggleOverlay}>
           <CloseFullscreenIcon fontSize="large" />
