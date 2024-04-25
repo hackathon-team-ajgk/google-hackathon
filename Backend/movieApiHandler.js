d625d374016b5bc1e65fcadfb83103d6e6886298
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-04-24 23:01:17 -0600

    feature: contains api request for home page, and minor fixes

--- Backend/movieApiHandler.js
+++ Backend/movieApiHandler.js
@@ -2,40 +2,40 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const API_READ_ACCESS_TOKEN = process.env.API_READ_ACCESS_TOKEN;
 const { json } = require("express");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 // Functions requiring API endpoints:
-// Working movie search 
+// Working movie search
 function searchForMovie(search_query) {
   try {
-    const formatted_query = search_query.replace(' ', '%20');
+    const formatted_query = search_query.replace(" ", "%20");
     const url = `https://api.themoviedb.org/3/search/movie?query=${formatted_query}&include_adult=false&language=en-US&page=1`;
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`
-      }
+        accept: "application/json",
+        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+      },
     };
-  
+
     return fetch(url, options)
-      .then(res => res.json())
-      .then(json => {
+      .then((res) => res.json())
+      .then((json) => {
         // console.log('API Response:', json); // Log the API response FOR TESTING THIS FUNC ONLY
         if (!json || !json.results) {
-          console.error('Invalid API Response:', json); // Log if response is invalid
-          throw new Error('Invalid API Response');
+          console.error("Invalid API Response:", json); // Log if response is invalid
+          throw new Error("Invalid API Response");
         }
         // Iterate over each movie result to stringify the genre_ids array
-        const formattedResults = json.results.map(movie => ({
+        const formattedResults = json.results.map((movie) => ({
           ...movie,
-          genre_ids: JSON.stringify(movie.genre_ids) // Stringify the genre_ids array
+          genre_ids: JSON.stringify(movie.genre_ids), // Stringify the genre_ids array
         }));
         // console.log(json)
         return json; // Return the JSON data from the resolved Promise
       })
-      .catch(err => {
-        console.error('Error fetching movie data:', err);
+      .catch((err) => {
+        console.error("Error fetching movie data:", err);
         throw err; // Throw the error to propagate it to the caller
       });
   } catch (error) {
@@ -45,45 +45,46 @@ function searchForMovie(search_query) {
 }
 // searchForMovie("harry potter")
 
-
-// Working get list of popular movies 
+// Working get list of popular movies
 function getPopularMovieHandler() {
   try {
-    const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=2';
+    const url =
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=2";
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`
-      }
+        accept: "application/json",
+        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+      },
     };
-    
+
     return fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.error('error:' + err));
+      .then((res) => res.json())
+      .then((res) => getMovieMetadataFromObject(res))
+      .catch((err) => console.error("error:" + err));
   } catch (error) {
-    console.error("Error getting popular movies", error)
+    console.error("Error getting popular movies", error);
+    throw error;
   }
 }
 // getPopularMovieHandler()
 
 // DOESNT WORK AS INTENDED. Not sure why
 function getWatchProviderHandler() {
-    const movie_id = '140607'
-    const url = `https://api.themoviedb.org/3/movie/${movie_id}/watch/providers`;
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`
-      }
-    };
-    
-    return fetch(url, options)
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.error('error:' + err));
+  const movie_id = "140607";
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}/watch/providers`;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+    },
+  };
+
+  return fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
 }
 // getWatchProviderHandler()
 
@@ -92,13 +93,13 @@ function getWatchProviderHandler() {
 async function getMovieMetadataFromObject(userInput) {
   return new Promise(async (resolve, reject) => {
     try {
-      console.log("Logging type of userInput")
-      console.log(typeof(userInput));
+      console.log("Logging type of userInput");
+      console.log(typeof userInput);
       // console.log(userInput);
       // const movie_obj = await searchForMovie(userInput);
 
       const movie_obj = userInput;
-      
+
       if (!movie_obj || !movie_obj.results) {
         console.error("No movie data found");
         reject("No movie data found");
@@ -107,53 +108,58 @@ async function getMovieMetadataFromObject(userInput) {
 
       // Mapping genre IDs to names
       const genreDict = {
-        "genres": [
-          { "id": 28, "name": "Action" },
-          { "id": 12, "name": "Adventure" },
-          { "id": 16, "name": "Animation" },
-          { "id": 35, "name": "Comedy" },
-          { "id": 80, "name": "Crime" },
-          { "id": 99, "name": "Documentary" },
-          { "id": 18, "name": "Drama" },
-          { "id": 10751, "name": "Family" },
-          { "id": 14, "name": "Fantasy" },
-          { "id": 36, "name": "History" },
-          { "id": 27, "name": "Horror" },
-          { "id": 10402, "name": "Music" },
-          { "id": 9648, "name": "Mystery" },
-          { "id": 10749, "name": "Romance" },
-          { "id": 878, "name": "Science Fiction" },
-          { "id": 10770, "name": "TV Movie" },
-          { "id": 53, "name": "Thriller" },
-          { "id": 10752, "name": "War" },
-          { "id": 37, "name": "Western" }
-        ]
+        genres: [
+          { id: 28, name: "Action" },
+          { id: 12, name: "Adventure" },
+          { id: 16, name: "Animation" },
+          { id: 35, name: "Comedy" },
+          { id: 80, name: "Crime" },
+          { id: 99, name: "Documentary" },
+          { id: 18, name: "Drama" },
+          { id: 10751, name: "Family" },
+          { id: 14, name: "Fantasy" },
+          { id: 36, name: "History" },
+          { id: 27, name: "Horror" },
+          { id: 10402, name: "Music" },
+          { id: 9648, name: "Mystery" },
+          { id: 10749, name: "Romance" },
+          { id: 878, name: "Science Fiction" },
+          { id: 10770, name: "TV Movie" },
+          { id: 53, name: "Thriller" },
+          { id: 10752, name: "War" },
+          { id: 37, name: "Western" },
+        ],
       };
 
       if (movie_obj.results && movie_obj.results.length > 0) {
-        const movieMetadata = movie_obj.results.map(movie => ({
+        const movieMetadata = movie_obj.results.map((movie) => ({
           movieId: movie.id,
           title: movie.title,
           releaseDate: movie.release_date,
           genreIds: JSON.stringify(movie.genre_ids), // Stringify genre IDs
-          genreNames: movie.genre_ids.map(genreId => { // Map genre IDs to names
-            const genre = genreDict.genres.find(genre => genre.id === genreId);
-            return genre ? genre.name : "Unknown Genre";
-          }).join(','), // Join genre names into a single string
+          genreNames: movie.genre_ids
+            .map((genreId) => {
+              // Map genre IDs to names
+              const genre = genreDict.genres.find(
+                (genre) => genre.id === genreId
+              );
+              return genre ? genre.name : "Unknown Genre";
+            })
+            .join(","), // Join genre names into a single string
           coverImage: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
           popularity: movie.popularity,
           averageRating: movie.vote_average,
           overview: movie.overview,
-          status: 'NULL',
-          userRating: 'NULL'
+          status: "NULL",
+          userRating: "NULL",
         }));
-        
+
         const newData = {
-          movie: movieMetadata
+          movie: movieMetadata,
         };
-      
+
         filterMovieData(newData);
-      
+
         resolve(newData);
       } else {
         // Handle the case where movie_obj.results is empty or undefined
@@ -171,8 +177,8 @@ async function getMovieMetadataFromObject(userInput) {
 async function getMovieMetadataFromString(userInput) {
   return new Promise(async (resolve, reject) => {
     try {
-      console.log("Logging type of userInput")
-      console.log(typeof(userInput));
+      console.log("Logging type of userInput");
+      console.log(typeof userInput);
       console.log(userInput);
       const movie_obj = await searchForMovie(userInput);
 
@@ -184,53 +190,58 @@ async function getMovieMetadataFromString(userInput) {
 
       // Mapping genre IDs to names
       const genreDict = {
-        "genres": [
-          { "id": 28, "name": "Action" },
-          { "id": 12, "name": "Adventure" },
-          { "id": 16, "name": "Animation" },
-          { "id": 35, "name": "Comedy" },
-          { "id": 80, "name": "Crime" },
-          { "id": 99, "name": "Documentary" },
-          { "id": 18, "name": "Drama" },
-          { "id": 10751, "name": "Family" },
-          { "id": 14, "name": "Fantasy" },
-          { "id": 36, "name": "History" },
-          { "id": 27, "name": "Horror" },
-          { "id": 10402, "name": "Music" },
-          { "id": 9648, "name": "Mystery" },
-          { "id": 10749, "name": "Romance" },
-          { "id": 878, "name": "Science Fiction" },
-          { "id": 10770, "name": "TV Movie" },
-          { "id": 53, "name": "Thriller" },
-          { "id": 10752, "name": "War" },
-          { "id": 37, "name": "Western" }
-        ]
+        genres: [
+          { id: 28, name: "Action" },
+          { id: 12, name: "Adventure" },
+          { id: 16, name: "Animation" },
+          { id: 35, name: "Comedy" },
+          { id: 80, name: "Crime" },
+          { id: 99, name: "Documentary" },
+          { id: 18, name: "Drama" },
+          { id: 10751, name: "Family" },
+          { id: 14, name: "Fantasy" },
+          { id: 36, name: "History" },
+          { id: 27, name: "Horror" },
+          { id: 10402, name: "Music" },
+          { id: 9648, name: "Mystery" },
+          { id: 10749, name: "Romance" },
+          { id: 878, name: "Science Fiction" },
+          { id: 10770, name: "TV Movie" },
+          { id: 53, name: "Thriller" },
+          { id: 10752, name: "War" },
+          { id: 37, name: "Western" },
+        ],
       };
 
       if (movie_obj.results && movie_obj.results.length > 0) {
-        const movieMetadata = movie_obj.results.map(movie => ({
+        const movieMetadata = movie_obj.results.map((movie) => ({
           movieId: movie.id,
           title: movie.title,
           releaseDate: movie.release_date,
           genreIds: JSON.stringify(movie.genre_ids), // Stringify genre IDs
-          genreNames: movie.genre_ids.map(genreId => { // Map genre IDs to names
-            const genre = genreDict.genres.find(genre => genre.id === genreId);
-            return genre ? genre.name : "Unknown Genre";
-          }).join(','), // Join genre names into a single string
+          genreNames: movie.genre_ids
+            .map((genreId) => {
+              // Map genre IDs to names
+              const genre = genreDict.genres.find(
+                (genre) => genre.id === genreId
+              );
+              return genre ? genre.name : "Unknown Genre";
+            })
+            .join(","), // Join genre names into a single string
           coverImage: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
           popularity: movie.popularity,
           averageRating: movie.vote_average,
           overview: movie.overview,
-          status: 'NULL',
-          userRating: 'NULL'
+          status: "NULL",
+          userRating: "NULL",
         }));
-        
+
         const newData = {
-          movie: movieMetadata
+          movie: movieMetadata,
         };
-      
+
         filterMovieData(newData);
-      
+
         resolve(newData);
       } else {
         // Handle the case where movie_obj.results is empty or undefined
@@ -250,20 +261,23 @@ function filterMovieData(movieMetadata) {
     // Iterate through each movie object
     for (const movie of movieMetadata.movie) {
       // If movie doesnt have genre, title, or movieId, remove movie from results.
-      const filteredMovies = movieMetadata.movie.filter(movie => {
-        return movie.genreNames !== '' && movie.title !== '' && !isNaN(movie.movieId);
+      const filteredMovies = movieMetadata.movie.filter((movie) => {
+        return (
+          movie.genreNames !== "" && movie.title !== "" && !isNaN(movie.movieId)
+        );
       });
       movieMetadata.movie = filteredMovies;
 
       // Replace empty strings and zeros with 'NULL' for specific properties
-      movie.releaseDate = movie.releaseDate === '' ? 'NULL' : movie.releaseDate;
-      movie.popularity = movie.popularity === 0 ? 'NULL' : movie.popularity;
-      movie.averageRating = movie.averageRating === 0 ? 'NULL' : movie.averageRating;
-      movie.overview = movie.overview === '' ? 'NULL' : movie.overview;
+      movie.releaseDate = movie.releaseDate === "" ? "NULL" : movie.releaseDate;
+      movie.popularity = movie.popularity === 0 ? "NULL" : movie.popularity;
+      movie.averageRating =
+        movie.averageRating === 0 ? "NULL" : movie.averageRating;
+      movie.overview = movie.overview === "" ? "NULL" : movie.overview;
 
       // Check if the cover image ends with '.jpg', otherwise replace it with 'NULL'
       if (!/\.jpg$/.test(movie.coverImage)) {
-        movie.coverImage = 'NULL';
+        movie.coverImage = "NULL";
       }
     }
   } catch (err) {
@@ -271,26 +285,29 @@ function filterMovieData(movieMetadata) {
   }
 }
 
-// Working recommendations (FROM MOVIE API) given genre input 
+// Working recommendations (FROM MOVIE API) given genre input
 async function getGenreRecommendations(genreID) {
-  
   const options = {
-    method: 'GET',
-    headers:{
-      accept: 'application/json',
-      Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`
-    }
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+    },
   };
 
-  const result = await fetch("https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&with_genres=" + genreID, options);
+  const result = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?language=en-US&page=1&with_genres=" +
+      genreID,
+    options
+  );
   const jsonResult = await result.json();
-  console.log(typeof(jsonResult));
+  console.log(typeof jsonResult);
 
-// Pass jsonResult directly to getMovieMetadataFromObject if it's an object
-  if (typeof(jsonResult) === "object") {
+  // Pass jsonResult directly to getMovieMetadataFromObject if it's an object
+  if (typeof jsonResult === "object") {
     const final_value = await getMovieMetadataFromObject(jsonResult);
     console.log("Returned back to calee");
-    console.log(final_value)
+    console.log(final_value);
     return final_value;
   } else {
     // Handle the case where jsonResult is not an object
@@ -300,32 +317,46 @@ async function getGenreRecommendations(genreID) {
 }
 
 // Working return trending movies (FROM MOVIE API)
-async function getTrendingMovies(){
-
+async function getTrendingMovies() {
   const options = {
-    method: 'GET',
-    headers:{
-      accept: 'application/json',
-      Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`
-    }
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
+    },
   };
 
-  const result = await fetch("https://api.themoviedb.org/3/trending/movie/day?language=en-US",options)
+  const result = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+    options
+  );
   const jsonResult = await result.json();
   // console.log(jsonResult)
 
-  const finalResult = await getMovieMetadataFromObject(jsonResult)
-  console.log(finalResult);
-  return result
+  const finalResult = await getMovieMetadataFromObject(jsonResult);
+  console.log("Trending Movies Final:", finalResult);
+  return finalResult;
 }
 
 // Below only for testing purposes
 async function viewMovieMetadata() {
-  x = await getMovieMetadataFromString("Harry potter").then(x => console.log(x))
+  x = await getMovieMetadataFromString("Harry potter").then((x) =>
+    console.log(x)
+  );
   // y = await getGenreRecommendations(10752) // War genre tag
   // z = await getTrendingMovies()
 }
-viewMovieMetadata()
+viewMovieMetadata();
+
+module.exports = {
+  searchForMovie,
+  getGenreRecommendations,
+  getMovieMetadataFromObject,
+  getMovieMetadataFromString,
+  getTrendingMovies,
+  filterMovieData,
+  getPopularMovieHandler,
+};
 
 /* 
 TO DO:
