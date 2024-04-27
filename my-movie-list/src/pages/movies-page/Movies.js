57309f745cff755f235b7800c5b243de4ec3bb36
kiaanb1 <122762099+kiaanb1@users.noreply.github.com>
2024-04-26 19:42:03 -0600

    added movie list code

--- my-movie-list/src/pages/movies-page/Movies.js
+++ my-movie-list/src/pages/movies-page/Movies.js
@@ -1,11 +1,49 @@
-import "./Movies.css";
 
 function Movies() {
-  return (
-    <>
-      <h1>Movies</h1>
-    </>
-  );
-}
+  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-export default Movies;
+  const fetchMovies = async (genre, popularity) => {
+
+  };
+
+  // Handle search functionality - fetch movies based on the search query
+  const handleSearch = async () => {
+    // API call to search for movies
+    // setMovies with the search result
+  };
+
+  // Function to select a movie and display more information
+  const selectMovie = (movie) => {
+    setSelectedMovie(movie);
+    // Possibly set state to show an overlay with more details
+  };
+
+  // Function to add movie to user's list
+  const addToUserList = (movie) => {
+    // Implement functionality to add movie to user's list
+  };
+
+  // Fetch all movies on component mount
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+    
+};
+  return (
+    <div className="app">
+      <SearchBar onSearch={handleSearch} setSearchQuery={setSearchQuery} />
+      <MovieFilters onFilterChange={fetchMovies} />
+      <MovieGrid 
+        movies={movies} 
+        onSelectMovie={selectMovie} 
+        onAddToUserList={addToUserList} 
+      />
+      {selectedMovie && (
+        <MovieOverlay movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+      )}
+    </div>
+  );
+
+export default Movies
