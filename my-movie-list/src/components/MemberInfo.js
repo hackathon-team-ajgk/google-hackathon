49164e8244c5cc063c1aeb2d2dab06719edcaf4b
Ernesto-B <ernesto.barreto@ucalgary.ca>
2024-04-29 21:54:38 -0600

    Added Ernesto's member info

--- my-movie-list/src/components/MemberInfo.js
+++ my-movie-list/src/components/MemberInfo.js
@@ -11,10 +11,10 @@ export const teamInfo = [
   {
     name: "Ernesto",
     role: "Back-end Lead",
-    image: "",
-    bio: "",
-    github: "",
-    linkedIn: "",
+    image: "https://media.tenor.com/o-wNCEq_6f0AAAAe/pompo-the-cinephile-typing-fast.png",
+    bio: "Software Engineering student at the University of Calgary. I love to read, mountain bike, travel, learn languages, and watch anime. Visit my LinkedIn to tell me what we can improve 🙋!",
+    github: "https://github.com/Ernesto-B",
+    linkedIn: "linkedin.com/in/ernesto-barreto-58a9b0258",
   },
   {
     name: "Gabriel Toro",
