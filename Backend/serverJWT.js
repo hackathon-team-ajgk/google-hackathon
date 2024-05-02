b8a1bcc131a388f28f3d1aa8afa430da30bcebfd
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-05-01 18:21:17 -0600

    changed cors origin in backend for deployment

--- Backend/serverJWT.js
+++ Backend/serverJWT.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["https://deploy-mern"],
+    origin: ["https://google-hackathon-frontend-beige.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
